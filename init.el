698f8b1435c1c02008952f52ec69cb6c00614d4c
yamamoto yohei <yamamoto@debian9-yama.jaist.ac.jp>
2019-06-23 15:54:04 +0900

    added init.el

---
+++ init.el
@@ -0,0 +1,63 @@
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+;; 改行コードを表示する
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
+
+;; 列数を表示する
+(column-number-mode t)
+
+;; 行数を表示する
+(global-linum-mode t)
+
+;; カーソルの点滅をやめる
+(blink-cursor-mode 0)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; ウィンドウ内に収まらないときだけ、カッコ内も光らせる
+(setq show-paren-style 'mixed)
+(set-face-background 'show-paren-match-face "grey")
+(set-face-foreground 'show-paren-match-face "black")
+
+;; スペース、タブなどを可視化する
+;;(global-whitespace-mode 1)
+
+;; スクロールは１行ごとに
+(setq scroll-conservatively 1)
+
+;; C-kで行全体を削除する
+(setq kill-whole-line t)
+
+;;; dired設定
+(require 'dired-x)
+
+;; "yes or no" の選択を "y or n" にする
+(fset 'yes-or-no-p 'y-or-n-p)
+(load-theme `wheatgrass)
+
+;; enable company
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; switch focus to Buff List after C-x C-b
+(define-key global-map [remap list-buffers] 'ibuffer)
+
+
+;; enable MELPA repository
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
